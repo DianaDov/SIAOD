634e47efbfb91e2f49526b48fc107f8a40bbcdce
DianaDov <71924758+hottchocolate@users.noreply.github.com>
2020-11-21 17:53:24 +0300

    Add files via upload

---
+++ siaod_4/src/com/company/Stack.java
@@ -0,0 +1,30 @@
+package com.company;
+
+//Класс-стек
+class Stack {
+    //Ссылка на вершину стека
+    private Node top;
+    //Конструктор для создания пустого стека
+    public Stack(){
+        top = null;
+    }
+    //Пустой ли стек?
+    public boolean isEmpty() { return top == null; }
+    //Вставляем элемент в стек
+    public void push(Node n){
+        n.next = top;
+        top = n;
+    }
+    //Очищаем вершину стека
+    public Node pop(){
+        Node toReturn = top;
+        top = top.next;
+        return toReturn;
+    }
+    //Смотрим вершину стека, но не удаляем её
+    public char peek(){
+        if (top == null) return '~';
+        return top.getOperation();
+    }
+}
+
